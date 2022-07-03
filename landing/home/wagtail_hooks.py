6b6d1739f4d07b34a0d7baccd6a0e635c6bf4c6e
ArthurJahn <stutrzbecher@gmail.com>
2022-07-03 11:58:31 -0300

    add validation fixes and styles

--- landing/home/wagtail_hooks.py
+++ landing/home/wagtail_hooks.py
@@ -1,3 +1,4 @@
+import re
 from django.contrib import messages
 from django.shortcuts import redirect
 from django.template.response import TemplateResponse
@@ -16,20 +17,21 @@ def process_form(page, request, *args, **kwargs):
             context = page.get_context(request, *args, **kwargs)
 
             # CPF Validation
-            cpf_valid = True
+            cpf_valid = False
             cpf_field = list(filter(lambda x: x.label.lower() == 'cpf', form))
             if len(cpf_field):
                 cpf_field = cpf_field[0]
-                cpf_valid = len(cpf_field) > 0 and not (cpf_field.value() and cpf_field.value().isdigit())
+                cpf_valid = len(cpf_field) > 0 and len(cpf_field.value()) == 11 and cpf_field.value().isdigit()
 
             # Email Validation
-            email_valid = True
+            email_valid = False
             email_field = list(filter(lambda x: x.label.lower() == 'e-mail', form))
-            email_confirmation_field = list(filter(lambda x: x.label.lower() == 'confirma e-mail', form))
+            email_confirmation_field = list(filter(lambda x: x.label.lower() == 'confirmação de e-mail', form))
             if len(email_field) and len(email_confirmation_field):
+                regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
                 email_field = email_field[0]
                 email_confirmation_field = email_confirmation_field[0]
-                email_valid = email_field == email_confirmation_field
+                email_valid = email_field.value() == email_confirmation_field.value() and re.fullmatch(regex, email_field.value())
 
             if form.is_valid() and cpf_valid and email_valid:
                 form_def.process_form_submission(form)
@@ -51,7 +53,7 @@ def process_form(page, request, *args, **kwargs):
                     messages.error(request, _('CPF inválido'), fail_silently=True)
 
                 if not email_valid:
-                    messages.error(request, _('E-mails não estão iguais'), fail_silently=True)
+                    messages.error(request, _('E-mail e confirmação de e-mail inválidos'), fail_silently=True)
 
                 if form_def.error_message:
                     messages.error(request, form_def.error_message, fail_silently=True)
