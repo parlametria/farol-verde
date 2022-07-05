135e90968d637549e0161cb18a57410c35b35da6
ArthurJahn <stutrzbecher@gmail.com>
2022-07-05 13:50:02 -0300

    add more side images

--- landing/landing/static/js/side-images.js
+++ landing/landing/static/js/side-images.js
@@ -1,12 +1,16 @@
 function setImg() {
   const current = Number(localStorage.getItem('curr_img'));
-  const next = (current < 5) ? current + 1 : 0;
+  const next = (current < 11) ? current + 1 : 0;
   
   localStorage.setItem('curr_img',next);
   document.querySelectorAll('.side-images img').forEach((img, i) => {
-    let className = (i === current) ? 'block' : 'none';
-    img.style.display = className;
+    if(i === current) {
+      img.style.opacity = '1';
+    } else {
+      img.style.opacity = '0';
+    }
   })
+  document.querySelector('.side-images').style.opacity='1';
 }
 setImg();
-setInterval(setImg, 25000);
+setInterval(setImg, 20000);
