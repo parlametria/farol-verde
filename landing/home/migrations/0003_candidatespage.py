77f1f47ded24ab56b80faa626ba68d143289af88
Jônatas G <jonatasgeek@gmail.com>
2022-07-06 14:50:01 -0300

    candidates page

---
+++ landing/home/migrations/0003_candidatespage.py
@@ -0,0 +1,41 @@
+from django.db import migrations, models
+import django.db.models.deletion
+import wagtail.core.blocks
+import wagtail.core.fields
+import wagtail.images.blocks
+from home.models import CandidatesPage
+from wagtail.core.models import Page
+
+def create_candidates_pages(apps, schema_editor):
+    home = Page.objects.get(title='Home')
+
+    candidates_page = CandidatesPage(title="Candidatos", slug="candidatos")
+
+    home.add_child(instance=candidates_page)
+    home.save()
+
+def remove_candidates_pages(apps, schema_editor):
+    home = Page.objects.get(title='Home')
+    candidates_page = home.get_children().get(slug="candidates")
+    candidates_page.delete()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailcore', '0066_collection_management_permissions'),
+        ('home', '0002_create_surveys_and_remove_fields'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='CandidatesPage',
+            fields=[
+                ('page_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='wagtailcore.page')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('wagtailcore.page',),
+        ),
+        migrations.RunPython(create_candidates_pages, remove_candidates_pages),
+    ]
