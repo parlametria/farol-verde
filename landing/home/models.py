20dd44c56016ed50fe797ca9d1a5c3c75c86a658
ArthurJahn <stutrzbecher@gmail.com>
2022-07-13 00:32:59 -0300

    add templatetags for loading menus from wagtail default promote settings

--- landing/home/models.py
+++ landing/home/models.py
@@ -1,5 +1,6 @@
 from wagtail.core.models import Page
 
+
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.admin.edit_handlers import StreamFieldPanel, FieldPanel
 from wagtailstreamforms.blocks import WagtailFormBlock
