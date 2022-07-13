7436a4b0ef1da81c61e01d2eb450fd6235fe8de3
Jônatas G <jonatasgeek@gmail.com>
2022-07-13 00:02:52 -0300

    update migrations
    Adjust migrations

---
+++ landing/candidate/migrations/0003_migrate.py
@@ -0,0 +1,28 @@
+
+from django.db import migrations, models
+from candidate.models import form_submission_link_candidate
+from wagtailstreamforms.models import FormSubmission
+
+def migrate_submissions_to_pages(apps, schema_editor):
+    submissions = FormSubmission.objects.all()
+    submissions = submissions.filter(form_id=1)
+    for submission in submissions:
+        form_submission_link_candidate(None, submission)
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        ('candidate', '0002_auto_20220711_1734'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='candidatepage',
+            name='manager_site',
+            field=models.URLField(blank=True, null=True),
+        ),
+        migrations.RunPython(migrate_submissions_to_pages),
+    ]
