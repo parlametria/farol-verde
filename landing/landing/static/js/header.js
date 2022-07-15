2134f41bbd1f51657f70d6c7b963ecb4a3e36fc4
Jônatas G <jonatasgeek@gmail.com>
2022-07-15 15:09:18 -0300

    create close btn and new header menu
    Set titles to center
    
    Set color to title
    
    Fix title size and weight

--- landing/landing/static/js/header.js
+++ landing/landing/static/js/header.js
@@ -1,5 +1,8 @@
 const headerLinks = document.querySelector('.header__links');
+const headerCloseBtn = document.querySelector('.header__close button')
 
 function openMenu() {
     headerLinks.classList.toggle('open')
 } 
+
+headerCloseBtn.addEventListener('click', openMenu)
