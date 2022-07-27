e98075d1e49c6f6f4a0d52e75927d1daa9ef25b6
Fabio Teixeira <fabio1079@gmail.com>
2022-07-27 18:07:33 -0300

    Put endpoint that uses from perfil.parlamentar in a separate url

--- landing/candidate/urls.py
+++ landing/candidate/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from candidate.views.perfil_parlamentar import votacoes_perfil_parlamentar_view
 from candidate.views.parlamentar import votacoes_parlamentar_view
 
 urlpatterns = [
-    path("votacoes/parlamentar/<int:id_candidate>", votacoes_parlamentar_view)
+    path("votacoes/perfil-parlamentar/<int:id_candidate>", votacoes_perfil_parlamentar_view),
+    path("votacoes/parlamentar/<int:id_candidate>", votacoes_parlamentar_view),
 ]
