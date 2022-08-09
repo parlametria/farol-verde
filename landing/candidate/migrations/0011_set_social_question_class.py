05af92a6e1ef30ef2dad84f8e582601f2e27a92d
ArthurJahn <stutrzbecher@gmail.com>
2022-08-09 18:58:37 -0300

    fix migration numbers

--- landing/candidate/migrations/0011_set_social_question_class.py
+++ landing/candidate/migrations/0011_set_social_question_class.py
@@ -26,7 +26,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('candidate', '00010_auto_20220804_2101'),
+        ('candidate', '0010_auto_20220804_2101'),
     ]
 
     operations = [
