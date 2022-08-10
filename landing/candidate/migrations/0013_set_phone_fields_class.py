d56218db0f050748b477ec5a637a385cde956638
jonatas1n <jonatasgeek@gmail.com>
2022-08-10 09:34:31 -0300

    base
    base
    
    create migration

---
+++ landing/candidate/migrations/0013_set_phone_fields_class.py
@@ -0,0 +1,44 @@
+from django.db import migrations, models
+from candidate.models import form_submission_link_candidate
+from wagtailstreamforms.models.form import Form
+
+social_questions = ['twitter', 'facebook', 'instagram', 'youtube']
+get_field_data = lambda field, data: field.__dict__['value'].get(data)
+
+def update_phone_help_text(apps, schema_editor):
+    form = Form.objects.get(title='Enquete')
+    fields = [field for field in form.fields
+        if get_field_data(field, 'label').lower() == 'Telefone do contato']
+    for field in fields:
+        help_text = get_field_data(field, 'help_text')
+        if 'phone' in help_text:
+            continue
+        field.__dict__['value'].update({'help_text': help_text + ' phone'})
+    form.save()
+
+def update_phone_help_text_reverse(apps, schema_editor):
+    form = Form.objects.get(title='Enquete')
+    fields = [field for field in form.fields
+        if get_field_data(field, 'label').lower() in social_questions]
+    for field in fields:
+        help_text = get_field_data(field, 'help_text')
+        help_text.replace('phone', '')
+        field.__dict__['value'].update({'help_text': help_text})
+    form.save()
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        ('candidate', '0012_set_opinions_blankable'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='candidatepage',
+            name='manager_site',
+            field=models.URLField(blank=True, null=True),
+        ),
+        migrations.RunPython(update_phone_help_text, update_phone_help_text_reverse),
+    ]
