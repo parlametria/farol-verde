525336b5e3c1f58a687fab1daacbd19f4dce310b
ArthurJahn <stutrzbecher@gmail.com>
2022-08-11 16:23:35 -0300

    fix gunicorn start

--- landing/run.sh
+++ landing/run.sh
@@ -5,7 +5,7 @@ source $(poetry env info --path)/bin/activate
 python manage.py migrate
 python manage.py findstatic .
 python manage.py collectstatic --noinput
-if [ $1 == prod ]; then
+if [ "$1" == "prod" ]; then
     poetry add gunicorn
     gunicorn landing.wsgi:application --bind 0.0.0.0:8000
 else
